911622dee7bc352af47209bb9d43166ce3378553
Joshua Honig <joshua@mallowfields.com>
2019-07-19 09:57:39 -0400

    Add internal common-utils module

---
+++ src/lib/netscaffold/common-utils.psm1
@@ -0,0 +1,80 @@
+$Accelerators = [PowerShell].Assembly.GetType("System.Management.Automation.TypeAccelerators")
+$CompletionCompleters = [System.Management.Automation.CompletionCompleters]
+$mClearCache = $CompletionCompleters.GetMethod('UpdateTypeCacheOnAssemblyLoad', [System.Reflection.BindingFlags]'NonPublic,Static')
+
+function Clear-TypeCache {
+  $mClearCache.Invoke($null, @( `
+    [System.AppDomain]::CurrentDomain, `
+    [System.AssemblyLoadEventArgs]::new([powershell].Assembly) `
+  ))
+}
+
+<#
+  Test whether the input value is null, or an empty or whitespace string
+#>  
+function Test-IsEmpty ([string]$Value) { return [string]::IsNullOrWhiteSpace($Value) }
+ 
+<#
+  Test whether the input value is a non-null string with no-whitespace characters
+#>  
+function Test-IsNotEmpty ([string]$Value) { return ![string]::IsNullOrWhiteSpace($Value) }
+
+<#
+  Define a new type accelerator (type alias)
+#>
+function Add-Accelerator {
+
+  [cmdletbinding()]
+  param([string]$Alias, $Type, [switch]$Force) 
+
+  $existing   = ($Accelerators::Get)
+  $targetType = ([type]$Type)
+  if ($existing.ContainsKey($Alias)) {
+    if ($Force) {
+      $Accelerators::Remove($Alias)
+    }
+    else {
+      if($targetType -eq $existing[$Alias]) {
+        # Ignore adding the same alias-type pair again. 
+        Write-Debug "Key '$Alias' is already associated with requested type $($targetType.FullName)"
+        return
+      }
+      
+      Write-Error "A type accelerator with the key '$Alias' already exists"
+      return
+    }
+  }
+  $Accelerators::Add($Alias, ([type]$Type))
+}
+
+<#
+Removing accelerators is not yet supported, despite the existence
+of the [TypeAccelerators]::Remove method.
+
+https://github.com/PowerShell/PowerShell/blob/b768d87081bbd04c361a9d27c23782726d4ecf12/src/System.Management.Automation/engine/parser/TypeResolver.cs#L850
+
+function Remove-Accelerator   {
+  
+  [cmdletbinding()]
+  param([string]$Alias)
+
+  if (($Accelerators::Get).ContainsKey($Alias)) {
+    $Accelerators::Remove($Alias)
+    Clear-TypeCache
+  } else {
+    Write-Error "Cannot find an acceleator with name '$Alias'"
+  }
+}
+ 
+Add-Accelerator list 'System.Collections.Generic.List`1'
+Add-Accelerator ienumerable 'System.Collections.Generic.IEnumerable`1'
+
+$OnRemoveScript = { 
+  Remove-Accelerator list
+  Remove-Accelerator ienumerable
+}
+
+$ExecutionContext.SessionState.Module.OnRemove += $OnRemoveScript
+#>
+
+Export-ModuleMember Test-IsEmpty, Test-IsNotEmpty, Add-Accelerator #, Remove-Accelerator
---
+++ tests/lib/common-utils.tests.ps1
@@ -0,0 +1,123 @@
+$srcRoot = [System.IO.Path]::Combine($PSScriptRoot, '..', '..', 'src', 'lib', 'netscaffold')
+Import-Module Pester
+
+if($null -ne (Get-Module common-utils)) { 
+  Write-Host "Unloading existing common-utils module" -ForegroundColor Cyan
+  Remove-Module common-utils
+}
+Import-Module (Join-Path $srcRoot 'common-utils.psm1')
+
+Describe 'common-utils' {
+
+  Context Test-IsEmpty {
+    It 'returns true for $null' {
+      $result = Test-IsEmpty $null
+      $result | Should -Be $true
+    }
+
+    It 'returns true for empty string' {
+      $result = Test-IsEmpty $([string]::Empty)
+      $result | Should -Be $true
+    }
+
+    It 'returns true for whitespace string' {
+      $result = Test-IsEmpty " `r`n `t "
+      $result | Should -Be $true
+    }
+
+    It 'returns false for non-empty string' {
+      $result = Test-IsEmpty a
+      $result | Should -Be $false
+    } 
+  }
+
+  Context Test-IsNotEmpty {
+    It 'returns false for $null' {
+      $result = Test-IsNotEmpty $null
+      $result | Should -Be $false
+    }
+
+    It 'returns false for empty string' {
+      $result = Test-IsNotEmpty $([string]::Empty)
+      $result | Should -Be $false
+    }
+
+    It 'returns false for whitespace string' {
+      $result = Test-IsNotEmpty " `r`n `t "
+      $result | Should -Be $false
+    }
+
+    It 'returns true for non-empty string' {
+      $result = Test-IsNotEmpty a
+      $result | Should -Be $true
+    } 
+  } 
+
+  Context Add-Accelerator {
+    It 'adds a new accelerator' {
+      # Accelerator does not exist yet
+      { [type]'map' } | Should -Throw
+
+      Add-Accelerator map 'System.Collections.Generic.Dictionary[String,Object]'
+
+      $expected = [System.Collections.Generic.Dictionary`2].MakeGenericType(@([string], [object]))
+      [type]'map' | Should -Be $expected
+    }
+
+    It 'adds does not overwrite an accelerator' {
+      # psobject accelerator is already set
+      [psobject].FullName | Should -Be 'System.Management.Automation.PSObject'
+
+      { Add-Accelerator psobject [string] -ErrorAction Stop } | Should -Throw
+
+      # psobject accelerator has not been changed
+      [psobject].FullName | Should -Be 'System.Management.Automation.PSObject'
+    }
+
+    It 'ignores adding the same alias-type pair more than once' {
+       # Accelerator does not exist yet
+       { [type]'map2' } | Should -Throw
+
+       Add-Accelerator map2 'System.Collections.Generic.Dictionary[String,Object]'
+ 
+       $expected = [System.Collections.Generic.Dictionary`2].MakeGenericType(@([string], [object]))
+       [type]'map2' | Should -Be $expected
+
+       { Add-Accelerator map2 'System.Collections.Generic.Dictionary[String,Object]' } | Should -Not -Throw
+       [type]'map2' | Should -Be $expected
+    }
+
+    It 'overwrites an existing accelerator if requested' {
+      # psobject accelerator is already set
+      [psobject].FullName | Should -Be 'System.Management.Automation.PSObject'
+
+      { Add-Accelerator psobject [string] -ErrorAction Stop } | Should -Throw
+
+      # psobject accelerator has not been changed
+      [psobject].FullName | Should -Be 'System.Management.Automation.PSObject'
+    }
+  }
+ 
+  <#
+  Removing accelerators is not yet supported
+  
+  Context Remove-Accelerator {
+    It 'Removes an existing accelerator' {
+      # Accelerator does not exist yet
+      { [type]'map2' } | Should -Throw
+
+      Add-Accelerator map2 'System.Collections.Generic.Dictionary[String,Object]'
+
+      $expected = [System.Collections.Generic.Dictionary`2].MakeGenericType(@([string], [object]))
+
+      # Now accelerator exists as defined
+      [type]'map2' | Should -Be $expected
+
+      Remove-Accelerator map2
+
+      # Accelerator is gone now 
+      { [type]'map2' } | Should -Throw
+    } 
+  }
+  #>
+}
