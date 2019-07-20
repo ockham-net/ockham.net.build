07f849d7fb3c1ddcf3c09b2b5765fc5aae6496c4
Joshua Honig <joshua@mallowfields.com>
2019-07-19 20:46:31 -0400

    Rename folders

---
+++ src/netscaffold/netscaffold.psm1
@@ -0,0 +1,17 @@
+Import-Module (Join-Path $PSScriptRoot (Join-Path modules common-utils.psm1))
+Import-Module (Join-Path $PSScriptRoot (Join-Path modules fs-utils.psm1))
+Import-Module (Join-Path $PSScriptRoot (Join-Path modules msbuild-evaluator.psm1))
+Import-Module (Join-Path $PSScriptRoot (Join-Path modules project-registry.psm1))
+Import-Module (Join-Path $PSScriptRoot (Join-Path modules solution-file.psm1))
+
+. (Join-Path $PSScriptRoot (Join-Path modules project-types.ps1)) | Out-Null
+
+Export-ModuleMember -Function `
+  Get-ProjectProperties, `
+  Add-ProjectType, `
+  Get-ProjectType, `
+  Resolve-ProjectType, `
+  New-Solution, `
+  Get-ProjectGraph, `
+  Add-SolutionFolder, `
+  Add-Project
